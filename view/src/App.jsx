ac2ba235d34ea8e2ab92615c887b8853a322c8ad
Matteo <msette8@gmail.com>
2023-03-14 20:11:44 +0100

    Edit with format

--- view/src/App.jsx
+++ view/src/App.jsx
@@ -1,21 +1,23 @@
 import './App.scss';
-import {useReducer} from 'react';
-import {Route, Routes} from 'react-router-dom';
+import { useReducer } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home.jsx';
 import Loan from './Pages/Loan.jsx';
 import Layout from './Pages/Layout.jsx';
-import {BasketContext} from './context/BasketContext.js';
-import {basketReducer} from "./reducer/basketReducer.js";
+import { BasketContext } from './context/BasketContext.js';
+import { basketReducer } from './reducer/basketReducer.js';
 
 function App() {
-    const [basketState, basketDispatch] = useReducer(basketReducer, {books: []})
+    const [basketState, basketDispatch] = useReducer(basketReducer, {
+        books: [],
+    });
     return (
-        <BasketContext.Provider value={{basketState, basketDispatch}}>
-            <div className="App">
+        <BasketContext.Provider value={{ basketState, basketDispatch }}>
+            <div className='App'>
                 <Routes>
-                    <Route path="/" element={<Layout/>}>
-                        <Route index element={<Home/>}/>
-                        <Route path="/loan" element={<Loan/>}/>
+                    <Route path='/' element={<Layout />}>
+                        <Route index element={<Home />} />
+                        <Route path='/loan' element={<Loan />} />
                     </Route>
                 </Routes>
             </div>
